12f7b784afed21f5c0a07010f64947f8bfd15816
Jay Lee <jahjaylee@gmail.com>
2014-04-02 21:30:56 -0500

    Swipe kinda works

--- 1k/ViewController.m
+++ 1k/ViewController.m
@@ -17,7 +17,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UISwipeGestureRecognizer *rightSwipe=[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    rightSwipe.direction=UISwipeGestureRecognizerDirectionRight;
+    
+    UISwipeGestureRecognizer *leftSwipe=[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    leftSwipe.direction=UISwipeGestureRecognizerDirectionLeft;
+    
+    [self.view addGestureRecognizer:leftSwipe];
+    [self.view addGestureRecognizer:rightSwipe];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)handleSwipe:(id)sender{
+    NSLog(@"Hello?");
+    [_mainImage setHidden:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +39,16 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)handlePan:(UIPanGestureRecognizer *)recognizer {
+    
+    CGPoint translation = [recognizer translationInView:self.view];
+    recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x,
+                                         recognizer.view.center.y + translation.y);
+    [recognizer setTranslation:CGPointMake(0, 0) inView:self.view];
+    
+}
+
+
 
 @end
